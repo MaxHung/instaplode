bce2b538be1396632f4d44e956a79338428d4954
MaxHung <maxkong2002@hotmail.com>
2017-03-09 19:30:06 +0800

    Update tree2.1.py

--- tree2.1.py
+++ tree2.1.py
@@ -6,23 +6,14 @@ class Tree(object):
 	def insert_l(self,a):
 		self.l=Tree(a)
 	def insert_r(self,b):
-	    self.r=Tree(b)
+		self.r=Tree(b)
 def travel(t):
 	lst=[]
-	c=0
 	if t==None:
-		return [None]
-	elif type(t) == int:
-		return [t]
+		return []
 	else:
-		lst=lst+travel(t.l)+travel(t.val)+travel(t.r)
-		for i in lst:
-			if i==None:
-				c=c+1
-		for n in range(0,c):
-			lst.remove(None)
+		lst=lst+travel(t.l)+[t.val]+travel(t.r)
 		return lst
-	return lst
 def build_Tree(n):
 	if n<=0 or type(n) != int:
 		raise ValueError("Input should be whole number.")
