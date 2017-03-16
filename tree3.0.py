80196298e844a4b268ecd533f4976e8ff1bea751
MaxHung <maxkong2002@hotmail.com>
2017-03-16 20:46:51 +0800

    Update tree3.0.py

--- tree3.0.py
+++ tree3.0.py
@@ -1,10 +1,10 @@
 def init(n):
 	return [0]*(2**n)
-def modify_l(l,i,x):
-		l[i*2+1]=x
-def modify_r(l,i,x):
-		l[(i+1)*2]=x
-lst=init(4)
+def modify_l(l, i, x):
+		l[i*2+1] = x
+def modify_r(l, i, x):
+		l[(i+1)*2] = x
+lst = init(4)
 print lst #prints [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
 modify_l(lst,5,10)
 print lst #prints[0,0,0,0,0,0,0,0,0,0,0,10,0,0,0,0,]
