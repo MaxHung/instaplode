c0b94e5d6bc66b16d635d7cddc08b055617bc313
MaxHung <maxkong2002@hotmail.com>
2017-03-16 20:52:03 +0800

    Update tree4.0.py

--- tree4.0.py
+++ tree4.0.py
@@ -1,33 +1,33 @@
 class Tree(object):
-	def __init__(self,val,l=None, r=None):
+	def __init__(self, val ,l=None, r=None):
 		self.val=val
 		self.l=l
 		self.r=r
-	def insert(self,x):
-			if x< self.val:
+	def insert(self ,x):
+			if x < self.val:
 				if type(self.l) == int or self.l == None:
-					self.l= Tree(x)
+					self.l = Tree(x)
 				else:
 					self.l.insert(x)
 			else:
 				if type(self.r) == int or self.r == None:
-					self.r=Tree(x)
+					self.r = Tree(x)
 				else:
 					self.r.insert(x)
 	def travel(self):
 		if self.l == None:
-			left=[]
+			left = []
 		else:
 			left=self.l.travel()
 		if self.r == None:
-			right=[]
+			right = []
 		else:
 			right=self.r.travel()
 		lst=left+[self.val]+right
 		return lst
 def sort_list(l):
-	t=Tree(l[0])
-	for i in range(1,len(l)):
+	t = Tree(l[0])
+	for i in range(1, len(l)):
 		t.insert(l[i])
 	return t.travel()
 print sort_list([9,8,7,6,5,4,3,2,1]) #returns [1,2,3,4,5,6,7,8,9]
